8e84b9a11fd6592ad5d13c34b198d938dfdc9a57
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-11-25 10:32:18 +0900

    03_02

--- main.cpp
+++ main.cpp
@@ -5,55 +5,43 @@
 
 // c++
 #include <iostream>
-#include <list>
-
-//* 駅
-struct Station {
-
-	const char* name;
-	int yearOpened;
-};
-
-//* 駅名表示
-void PrintStations(const std::list<Station>& stations, int year) {
-
-	std::cout << year << "年の駅一覧:" << std::endl;
-	for (const auto& station : stations) {
-
-		if (station.yearOpened <= year) {
-			std::cout << "- " << station.name << std::endl;
-		}
-	}
-}
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 ///=================================================================================
 /// main
 ///=================================================================================
 int main() {
 
-	// 駅名リスト
-	std::list<Station> stations = {
-		{"Tokyo", 1872}, {"Kanda", 1919}, {"Akihabara", 1925}, {"Okachimachi", 1925},
-		{"Ueno", 1883}, {"Uguisudani", 1912}, {"Nippori", 1905}, {"Nishi-Nippori", 1971},
-		{"Tabata", 1896}, {"Komagome", 1910}, {"Sugamo", 1903}, {"Otsuka", 1903},
-		{"Ikebukuro", 1903}, {"Mejiro", 1910}, {"Takadanobaba", 1910}, {"Shin-Okubo", 1914},
-		{"Shinjuku", 1885}, {"Yoyogi", 1906}, {"Harajuku", 1906}, {"Shibuya", 1885},
-		{"Ebisu", 1906}, {"Meguro", 1906}, {"Gotanda", 1911}, {"Osaki", 1901},
-		{"Shinagawa", 1872}, {"Tamachi", 1909}, {"Hamamatsucho", 1909}, {"Shimbashi", 1909},
-		{"Yurakucho", 1910}, {"Takanawa Gateway", 2020}
-	};
+	// txtファイルパス
+	const std::string filePath = "PG3_2024_03_02.txt";
+	std::ifstream inputFile(filePath);
 
-	// 1970年の駅一覧
-	std::cout << "------------------" << std::endl;
-	PrintStations(stations, 1970);
+	std::vector<std::string> emails;
 
-	// 2019年の駅一覧
-	std::cout << "------------------" << std::endl;
-	PrintStations(stations, 2019);
+	std::string line;
+	while (std::getline(inputFile, line)) {
 
-	// 2022年の駅一覧
-	std::cout << "------------------" << std::endl;
-	PrintStations(stations, 2022);
+		// コンマ区切りで追加
+		std::stringstream ss(line);
+		std::string email;
+		while (std::getline(ss, email, ',')) {
+
+			emails.push_back(email);
+		}
+	}
+	inputFile.close();
+
+	// 若い順
+	std::sort(emails.begin(), emails.end());
+
+	std::cout << "学籍番号順: " << std::endl;
+	for (const auto& email : emails) {
+
+		std::cout << email << std::endl;
+	}
 
 	return 0;
 
